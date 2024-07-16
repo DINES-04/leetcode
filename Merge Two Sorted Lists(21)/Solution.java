e2dd8c4428d07b7d4c8f97c849aa1c236f52ecc7
DINES-01 <dines.s16102004@gmail.com>
2024-07-16 09:21:41 +0530

    leetcode no:21 completed

---
+++ Merge Two Sorted Lists(21)/Solution.java
@@ -0,0 +1,68 @@
+import java.util.*;
+class Solution {
+    static class ListNode {
+     	int val;
+	ListNode next;
+     	ListNode() {}
+     	ListNode(int val) { this.val = val; }
+    	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode ln = new ListNode(0);
+        ListNode current = ln;
+        ListNode p = list1;
+        ListNode q = list2;
+        while (true) {
+            if (p == null && q == null) {
+                break;
+            }
+            if (p != null && (q == null || p.val <= q.val)) {
+                current.next = p;
+                p = p.next;
+            } else {
+                current.next = q;
+                q = q.next;
+            }
+            current = current.next;
+        }
+        return ln.next;
+    }
+     public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public static ListNode createnode(int[] arr){
+	ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        for (int val : arr) {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+	return dummy.next;
+    }
+    public static void main(String args[]){
+	Scanner sc= new Scanner(System.in);
+	System.out.print("Enter the no.of element 1st list:");
+	int len= sc.nextInt();
+	int[] arr1 = new int[len];
+	 System.out.print("Enter the elements: ");
+        for (int i = 0; i < len; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        ListNode head1=createnode(arr1);
+	System.out.print("Enter the no.of element 2nd list:");
+	int len2= sc.nextInt();
+	int[] arr2 = new int[len];
+	 System.out.print("Enter the elements: ");
+        for (int j = 0; j < len2; j++) {
+            arr2[j] = sc.nextInt();
+        }
+        ListNode head2=createnode(arr2);
+	ListNode res= mergeTwoLists(head1,head2);
+	printList(res);
+    }
+}
